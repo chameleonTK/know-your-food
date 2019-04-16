8afc54d61f3b3c63bef5fb63e5c4767db30a8ab4
pn37 <pn37@pc5-003-l.cs.st-andrews.ac.uk>
2019-04-16 21:34:57 +0100

    bbchart

---
+++ BubbleChart.js
@@ -0,0 +1,79 @@
+var BubbleChart = function(domSelection, data, options) {
+    var vm = this;
+    var height = options.height;
+    var width = 295;
+    var marginTop = 30;
+
+    var keys = options.keys;
+    var _pos = options.pos
+    var _offset = options.offset;
+
+    this.svg = d3.select(domSelection)
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+
+    console.log(data[0])
+    _allergy = _.map(keys, (label, name) => {
+        var count = _.countBy(data, (d) => d._raw[name]=="True")
+
+        //TODO: resampling data
+        if (count["true"] === undefined || count["true"]===0) {
+            count["true"] = _.random(10, 100, false);
+            count["false"] -= count["true"]
+        }
+
+        return {
+            name,
+            count,
+            label,
+        }
+    })
+
+    _x = d3.scaleLinear()
+    .domain([0, data.length]).nice()
+    .range([20, 45])
+
+    
+
+    _ele = this.svg.selectAll("circle")
+    .data(_allergy)
+    .enter()
+    .append("g")
+    .attr("transform", (d, i) => "translate("+(_pos[i].x*width)+","+(_pos[i].y*height - marginTop)+")")
+    .on("mouseover", _mouseover)					
+    .on("mouseout", _mouseout)
+    
+    _ele.append("circle")
+    .attr('r', (d) => _x(d.count["true"]))
+
+    _hover = _ele.append("circle")
+    .attr('r', (d) => _x(d.count["true"])+5)
+    .attr("class", "hover-cir")
+    .style('display', "none")
+
+
+    _ele
+    .append("text")
+    .attr("dy", (d) => 55)
+    .attr("dx", (d, i) => _offset[i])
+    .text(d => d.label)
+
+
+    this.svg
+    .append("text")
+    .attr("dx", width-65)
+    .attr("dy", height-20)
+    .text("30%")
+    .style("font-size", "28px")
+    .style("fill", "rgb(137, 223, 196)")
+
+    function _mouseover(d) {
+        d3.select(this).select(".hover-cir").style('display', null)
+    }
+
+    function _mouseout(d) {
+        d3.select(this).select(".hover-cir").style('display', "none")
+    }
+}
--- app.js
+++ app.js
@@ -14,8 +14,7 @@ var VisualiseApp = function() {
             carbohydrate: vm.val(item, "Carbohydrate, by difference[Output_Value]", "Carbohydrate, by difference[Output_UOM]", "Carbohydrate, by difference[Derivation_Code]"),
             fat: vm.val(item, "Fatty acids, total trans[Output_Value]", "Fatty acids, total trans[Output_UOM]", "Fatty acids, total trans[Derivation_Code]") + vm.val(item, "Fatty acids, total saturated[Output_Value]", "Fatty acids, total saturated[Output_UOM]", "Fatty acids, total saturated[Derivation_Code]"),
             fiber: vm.val(item, "Fiber, total dietary[Output_Value]", "Fiber, total dietary[Output_UOM]", "Fiber, total dietary[Derivation_Code]"),
-
-
+            _raw: item,
         }
     }
 
@@ -30,6 +29,84 @@ var VisualiseApp = function() {
         return 0;
     }
 
+    vm.getAllergicChart = function(data) {
+        keys = {
+            allergy_eggs_milk: "Eggs & Milk",
+            allergy_treenuts: "Treenuts",
+            allergy_fish_shellfish: "Fish & Shellfish",
+            allergy_wheat: "Wheat",
+            allergy_soy_peanuts: "Peanuts & Soy",
+        };
+    
+        labelOffsets = [
+            -86*0.5,
+            -63*0.5,
+            -112*0.5,
+            -46*0.5,
+            -105*0.5,
+        ]
+
+        pos = [
+            {x: 1.25*0.25, y: 0.25},
+            {x: 2.75*0.25, y: 0.25},
+            {x: 1.25*0.25, y: 2.125*0.25},
+            {x: 2.75*0.25, y: 2.125*0.25},
+            {x: 1.25*0.25, y: 3.25*0.25},
+        ]
+
+        return (new BubbleChart("#chart-allergic", data, {
+            keys,
+            pos,
+            offset: labelOffsets,
+            height: 385,
+        }))
+    }
+
+    vm.getPreferenceChart = function(data) {
+        keys = {
+            has_vegetable: "Vegetable",
+            has_beef: "Beef",
+            has_egg: "Eggs",
+            has_chicken: "Chicken",
+            has_lamp: "Lamp",
+            has_pork: "Pork",
+            has_seafood: "Seafood",
+            has_nuts: "Nuts",
+            
+        };
+
+       
+    
+        labelOffsets = [
+            -70*0.5,
+            -34*0.5,
+            -38*0.5,
+            -59*0.5,
+            -41*0.5,
+            -55.3*0.5,
+            -60*0.5,
+            -35*0.5,
+        ]
+
+        pos = [
+            {x: 1.25*0.25, y: 0.2},
+            {x: 2.75*0.25, y: 0.2},
+            {x: 1.25*0.25, y: 2.125*0.2},
+            {x: 2.75*0.25, y: 2.125*0.2},
+            {x: 1.25*0.25, y: 3.25*0.2},
+            {x: 2.75*0.25, y: 3.25*0.2},
+            {x: 1.25*0.25, y: 4.375*0.2},
+            {x: 2.75*0.25, y: 4.375*0.2},
+        ]
+
+        return (new BubbleChart("#chart-preference", data, {
+            keys,
+            pos,
+            offset: labelOffsets,
+            height: 450,
+        }))
+    }
+
     vm.visualize = function(data) {
         var data = vm.preprocessing(data)
         vm.filterCharts = {
@@ -37,23 +114,11 @@ var VisualiseApp = function() {
             carbohydrate: new AreaChart("#chart-carbohydrate", data, "carbohydrate"),
             fat: new AreaChart("#chart-fat", data, "fat"),
             fiber: new AreaChart("#chart-fiber", data, "fiber"),
+            allergic: vm.getAllergicChart(data),
+            prefernce: vm.getPreferenceChart(data),
         };
     }
 }
 
 
 var app = new VisualiseApp();
-
-
-// app.views.charts
-// .polygonChart()
-// .id("bar09")
-// .dimension(app.data.calm)
-// .verticalParameter("value")
-// .width($("#chart09").parent().width())
-// .height(96)
-// .initialBrushExtent(u.bar09)
-// .tooltipScale(["not calm","calm"])
-// .brushed(brushed)
-// .hoverCallback(hover)
-// .unhoverCallback(unhover)
